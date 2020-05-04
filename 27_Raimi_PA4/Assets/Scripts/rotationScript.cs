483926d6d5a5e488189fe0398bfb1987b395933f
iiAmSpeed <60515448+iiAmSpeed@users.noreply.github.com>
2020-05-04 17:23:32 +0800

    Added a unity project

---
+++ 27_Raimi_PA4/Assets/Scripts/rotationScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 25, 0);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * 5 * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * 5 * Time.deltaTime;
+        }
+    }
+}
